4fff099770e8af80979b48c406a577117feb5046
zun43d <zunaedahmed.info@gmail.com>
2022-11-01 02:36:34 +0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import 'regenerator-runtime/runtime'
+
+import { UALProvider, withUAL } from 'ual-reactjs-renderer'
+import { Wax } from '@eosdacio/ual-wax'
+import { Anchor } from 'ual-anchor'
+
+const myChain = {
+	chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
+	rpcEndpoints: [
+		{
+			protocol: 'https',
+			host: 'wax.dfuse.eosnation.io',
+			port: '',
+		},
+	],
+}
+
+const wax = new Wax([myChain], { appName: 'My App' })
+const anchor = new Anchor([myChain], { appName: 'My App' })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const MyUALConsumer = withUAL(Component)
+
+	// return <Component {...pageProps} />
+	return (
+		<UALProvider
+			chains={[myChain]}
+			authenticators={[wax, anchor]}
+			appName="My App"
+		>
+			<MyUALConsumer {...pageProps} />
+		</UALProvider>
+	)
 }
 
 export default MyApp
