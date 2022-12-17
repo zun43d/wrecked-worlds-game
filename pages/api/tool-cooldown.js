542662d1a5b67e1497514efca7f310e8ba6bfe3e
Zunaed Ahmed <zunaedahmed.info@gmail.com>
2022-12-17 22:23:59 +0600

    Subtle changes

---
+++ pages/api/tool-cooldown.js
@@ -0,0 +1,28 @@
+import { JsonRpc } from 'eosjs'
+import fetch from 'node-fetch'
+
+const api = process.env.NEXT_PUBLIC_API_ENDPOINT
+
+export default async function handler(req, res) {
+	if (req.method === 'GET') {
+		const { toolId, wallet } = req.query
+		const rpc = new JsonRpc(api, { fetch })
+
+		const { rows: tools } = await rpc.get_table_rows({
+			json: true,
+			code: 'wreckminings',
+			scope: wallet,
+			table: 'tools',
+			key_type: 'i64',
+			index_position: 1,
+			lower_bound: toolId,
+			upper_bound: toolId,
+			limit: 100,
+		})
+
+		const cooldownUntil =
+			new Date(tools[0].cooldown_until).toLocaleString() + ' GMT'
+
+		res.status(200).json(cooldownUntil)
+	}
+}
